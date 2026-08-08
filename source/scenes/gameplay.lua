8e778826edf76ae91213bf4549ec46393e66569e
Carlos Tacón <carlosjtacon@gmail.com>
2026-08-09 10:43:09 +1200

    controls to build your shape

--- source/scenes/gameplay.lua
+++ source/scenes/gameplay.lua
@@ -1,37 +1,184 @@
+import "CoreLibs/object"
+import "CoreLibs/crank"
+
 local gfx <const> = playdate.graphics
 
-local player = {
-  x = 10,
-  y = 10,
-  w = 16,  -- width
-  h = 16,  -- height
-  s = 200, -- speed in px/s
+local rows <const> = 8
+local gridSize <const> = DISPLAY_HEIGHT / rows
+local cols <const> = math.ceil(DISPLAY_WIDTH / gridSize)
+
+local ticksPerRevolution <const> = 6
+
+local map = {
+  player = {},
+  puzzle = {},
 }
+
+controls_default = {
+  up = playdate.kButtonUp,
+  down = playdate.kButtonDown,
+  left = playdate.kButtonLeft,
+  right = playdate.kButtonRight,
+  addTop = 1,
+  addBottom = -1,
+  removeTop = playdate.kButtonB,
+  removeBottom = playdate.kButtonA,
+}
+
+controls_swapped = {
+  up = playdate.kButtonUp,
+  down = playdate.kButtonDown,
+  left = playdate.kButtonLeft,
+  right = playdate.kButtonRight,
+  addTop = 1,
+  addBottom = -1,
+  removeTop = playdate.kButtonB,
+  removeBottom = playdate.kButtonA,
+}
+
+controls = {}
+
+function init()
+  controls = controls_default
+
+  -- init the map for player and puzzle
+  for i = 1, rows do
+    map.player[i] = {}
+    map.puzzle[i] = {}
+    for j = 1, cols do
+      map.player[i][j] = 0
+      map.puzzle[i][j] = 0
+    end
+  end
+  map.player[1][1] = 1
+
+  -- printTable(map.player)
+end
+
+function drawGame()
+  gfx.clear()
+  playdate.graphics.setDrawOffset(-gridSize, -gridSize)
+  for i = 1, rows do
+    for j = 1, cols do
+      if map.player[i][j] == 1 then
+        gfx.fillRect(j * gridSize + 1, i * gridSize + 1, gridSize -2, gridSize -2)
+      end
+      if map.puzzle[i][j] == 1 then
+        -- gfx.setColor(gfx.kColorBlack)
+        gfx.fillRect(j * gridSize + 1, i * gridSize + 1, gridSize -2, gridSize -2)
+        -- gfx.setColor(gfx.kColorWhite)
+        -- gfx.fillRect(j * gridSize + 1, i * gridSize + 1, gridSize -2, gridSize -2)
+      end
+    end
+  end
+
+end
+
+local function getFirstMatch(matrix, value)
+  for j = 1, cols do
+    for i = 1, rows do
+      if matrix[i][j] == value then
+        return i, j
+      end
+    end
+  end
+end
+
+local function getLastMatch(matrix, value)
+  for j = 1, cols do
+    for i = rows, 1, -1 do
+      if matrix[i][j] == value then
+        return i, j
+      end
+    end
+  end
+end
+
+function updatePlayer(prevMap)
+  local crankTicks = playdate.getCrankTicks(ticksPerRevolution)
+
+  if crankTicks == controls.addTop then
+    i, j = getFirstMatch(map.player, 0)
+    map.player[i][j] = 1
+  end
+
+  if crankTicks == controls.addBottom then
+    i, j = getLastMatch(map.player, 0)
+    map.player[i][j] = 1
+  end
+
+  if playdate.buttonJustPressed(controls.removeTop) then
+    i, j = getFirstMatch(map.player, 1)
+    map.player[i][j] = 0
+  end
+  --
+  if playdate.buttonJustPressed(controls.removeBottom) then
+    i, j = getLastMatch(map.player, 1)
+    map.player[i][j] = 0
+  end
+
+  if playdate.buttonJustPressed(controls.left) then
+    for i = 1, rows do
+      for j = 1, cols do
+        if j == #map.player[i] then
+          map.player[i][j] = 0
+        else
+          map.player[i][j] = prevMap.player[i][j+1]
+        end
+      end
+    end
+  end
+
+  if playdate.buttonJustPressed(controls.right) then
+    for i = 1, rows do
+      for j = 2, cols do
+          map.player[i][j] = prevMap.player[i][j-1]
+      end
+    end
+  end
+
+  if playdate.buttonJustPressed(controls.up) then
+    for i = 1, rows do
+      for j = 1, cols do
+        if i == #map.player then
+          map.player[i][j] = 0
+        else
+          map.player[i][j] = prevMap.player[i+1][j]
+        end
+      end
+    end
+  end
+
+  if playdate.buttonJustPressed(controls.down) then
+    for i = 1, rows do
+      for j = 1, cols do
+        if i == 1 then
+          map.player[i][j] = 0
+        else
+          map.player[i][j] = prevMap.player[i-1][j]
+        end
+      end
+    end
+  end
+
+end
+
+local function updatePuzzle(prevMap)
+
+end
 
 local function update(dt)
   SaveData.playtime += dt
+  local prevMap = table.deepcopy(map)
 
-  if playdate.buttonIsPressed(playdate.kButtonLeft) then
-    player.x -= player.s * dt
-  end
-  if playdate.buttonIsPressed(playdate.kButtonRight) then
-    player.x += player.s * dt
-  end
-  if playdate.buttonIsPressed(playdate.kButtonUp) then
-    player.y -= player.s * dt
-  end
-  if playdate.buttonIsPressed(playdate.kButtonDown) then
-    player.y += player.s * dt
-  end
+  updatePlayer(prevMap)
+  updatePuzzle(prevMap)
 
-  if playdate.buttonJustPressed(playdate.kButtonB) then
-    PlaySFX("B3")
-    SwitchScene(SCENE.MAIN_MENU)
-  end
+  drawGame()
 
-  gfx.clear()
-  gfx.fillRect(player.x, player.y, player.w, player.h)
 end
+
+init()
 
 local scene = {
   update = update,
